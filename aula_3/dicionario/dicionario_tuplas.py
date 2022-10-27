633db85d4a50e484d05f0019cddca5d57e7b30df
sidneygotx <111944390+sidneygotx@users.noreply.github.com>
2022-10-27 00:55:17 -0300

    teste 2

--- aula_3/dicionario/dicionario_tuplas.py
+++ aula_3/dicionario/dicionario_tuplas.py
@@ -5,7 +5,7 @@ tupla = list(enumerate(emails))
 
 for chave in range(0, len(tupla)):
     print("email: ", tupla[chave][1])
-    usuario[tupla[chave]]=[input("Digite o nome"), input("digite o nível")]
+    usuario[tupla[chave]]=[input("Digite o nome: "), input("digite o nível")]
 
 for chave, dado in usuario.items():
     print("usuario: ", chave[0])
